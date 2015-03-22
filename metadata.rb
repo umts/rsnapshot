c4f6735bf86417189e431574beceff11fce7df15
Matt Moretti <werebus@gmail.com>
2015-03-22 18:03:55 -0400

    Some project skeleton work

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
 maintainer       "Finn GmbH"
 maintainer_email "info@finn.de"
 license          "MIT"
+name             "rsnapshot"
 description      "rsnapshot"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.0"
 
 %w{ ubuntu debian}.each do |os|
   supports os
---
+++ spec/unit/recipies/client_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'rsnapshot::client' do
+
+  context 'When all attributes are default, on an unspecified platform' do
+
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      chef_run # This should not raise an error
+    end
+  end
+end
